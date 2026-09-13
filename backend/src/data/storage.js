b2b25f72534bf46e138a2769b616c9750b0db993
Martin Alejandro Nuñez <martinalejandronuniez@gmail.com>
2026-09-13 20:53:15 -0300

    mensaje

--- backend/src/data/storage.js
+++ backend/src/data/storage.js
@@ -4,13 +4,6 @@ const path = require('path');
 const mediaDir = path.join(__dirname, '../../media/bicicletas');
 fs.mkdirSync(mediaDir, { recursive: true });
 
-const users = [];
-const products = [];
-const pedidos = [];
-
 module.exports = {
   mediaDir,
-  users,
-  products,
-  pedidos,
 };
