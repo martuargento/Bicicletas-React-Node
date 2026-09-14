d442a1dafdb393d0b4587c09b928b1f0220d97eb
Martin Alejandro Nuñez <martinalejandronuniez@gmail.com>
2026-09-14 01:27:16 -0300

    mensaje

--- backend/src/data/storage.js
+++ backend/src/data/storage.js
@@ -1,11 +1,30 @@
 //este archivo prepara la carpeta donde se guardan las imágenes de los productos.
 
+
+//fs significa file system, es un modulo nativo de node para trabajar con archivos, carpetas,
+//creacion, lectura, modificacion, eliminacion, etc.
 const fs = require('fs');
+
+//path tambien es un modulo nativo de node, sirve para contruir rutas correctamente
 const path = require('path');
 
+
+//aca construimos la ruta de las imagenes
+//esta linea crea la ruta absoluta hacia backend/media/bicicletas
 const mediaDir = path.join(__dirname, '../../media/bicicletas');
+
+
+//aca creamos la carpeta si no existe, (en este caso crearia media/bicicletas)
+//si la carpeta ya existe no pasa nada porque usamos recursive: true y evita ese error
 fs.mkdirSync(mediaDir, { recursive: true });
 
+
+// exportamos la variable creada, mediaDir
+//que es donde van a estar las imagenes
+//para que otros achivos puedan usarla
+//en este caso el archivo que la va a usar va a ser routes --> product.routes.js
+//ahi mediante su codigo va a hacer que cuando el usuario suba una imagen, la guarde en
+//la carpeta indicada por mediaDir
 module.exports = {
   mediaDir,
 };
--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,5 +1,26 @@
+//este es el archivo que crea y exporta el cliente que permite comunicarse con
+//la base de datos mediante Prisma
+
+
+//importamos PrismaClient desde el paquete generado por Prisma
+//PrismaClient es una clase que conoce:
+//• La conexion definida en prisma --> schema.prisma
+//• los modelos de la base de datos
+//• las operaciones disponibles sobre cada modelo
 const { PrismaClient } = require('@prisma/client');
 
+
+//creamos una instancia de este cliente PrismaClient
+//esta instancia va a permitir hacer consultas a la base de datos como:
+//• prisma.user.findUnique(...)
+//• prisma.user.create(...)
+//• prisma.producto.findMany(...)
+//• prisma.producto.create(...)
+//• prisma.pedido.findMany(...)
+//basicamente va a estar transformando nuestras solicitudes a la base de datos, en codigo sql
+//que entiende la base de datos
 const prisma = new PrismaClient();
 
+
+//exportamos esa instancia para que otros archivos puedan usarla
 module.exports = { prisma };
