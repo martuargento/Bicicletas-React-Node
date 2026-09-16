a9f84f773b05e11c6f2c3f2d6b24940fdcbd82c6
Martin Alejandro Nuñez <martinalejandronuniez@gmail.com>
2026-09-16 14:35:52 -0300

    Mensaje

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -73,7 +73,7 @@ function serializeUser(user) {
 
 //registrar un nuevo usuario
 //Esta función se ejecuta cuando llega esta solicitud:
-//POST /api/auth/registro
+// • POST /api/auth/registro
 
 //Es async porque va a realizar operaciones en la base de datos que pueden tardar
 async function registro(req, res) {
@@ -131,7 +131,7 @@ async function registro(req, res) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
 
-//ahora que creamos el usuario en la base de datos
+//aca creamos el usuario en la base de datos
   const newUser = await prisma.user.create({
     data: {
       username,
@@ -164,32 +164,120 @@ async function registro(req, res) {
 
 
 //loguearse
+//aca declaramos la funcion de la controladora que se encarga
+//de manejar la peticion al endpoint
+// • POST /api/auth/Login
+
+//la funcion es asincronica porque puede tardar ya que va a comunicarse con la base de datos
+//req = todo lo que llega del cliente
+//res = la respuesta que vamos a devolver desde el backend
 async function login(req, res) {
+  //extramos el usuario y contraseña que llego en la peticion
   const username = String(req.body.username || '').trim();
   const password = String(req.body.password || '');
 
+
+  //buscamos el usuario en la base de datos a traves de su username
+  //ese usuario se guarda en user
+  //si no existe user queda en null
   const user = await prisma.user.findUnique({
     where: { username },
   });
 
+  //si user no existe, osea.. si es null
+  //entra al if
+  //y retornamos un status 401, con el mensaje de error "credenciales invalidas"
   if (!user) {
     return res.status(401).json({ error: 'Credenciales inválidas.' });
   }
 
+  //si llegamos aca es porque se encontro el username en la base de datos
+  //aca vamos a ver si la contraseña que llego en la peticion
+  //coincide con la contraseña guardada en la base de datos, de ese username
+  //la comparacion la va a hacer bcrypt, ya que la contraseña que esta guardada
+  //en la base de datos para ese usuario, esta encriptada en forma de hash
+  //entonces esta funcion bcrypt.compare()
+  //se va a encargar de hacer esa comparacion entre la contraseña que vino en la peticion
+  //con el hash guardado en la base de datos
+  //va a devolver true si coincide
+  //y false si no coincide
+
+  //guardamos ese resultado en la constante passwordOk
   const passwordOk = await bcrypt.compare(password, user.password);
+
+
+  //si la contraseña no coincide
+  //retornamos un status 401, con el mensaje de error "credenciales invalidas"
   if (!passwordOk) {
     return res.status(401).json({ error: 'Credenciales inválidas.' });
   }
 
+
+  //si llegamos aca es porque se encontro el username en la base de datos
+  //y la contraseña que vino en la peticion, tambien era correcta
+  //asi que en este punto ya esta validado que el usuario es valido
+  
+  //asi que generamos un token JWT para ese usuario
+  //asi puede acceder a los otros endpoints en los que es obligatorio tener un Token JWT
   const tokens = generarToken(user);
 
+
+  // lo siguiente hace que el backend responda con:
+
+  // • status 200, todo salio bien
+  // • un objeto user con los datos del usuario creado
+  // • tokens: token de acceso y refresh, generado por la funcion generarToken()
   return res.status(200).json({
     user: serializeUser(user),
     tokens,
   });
 }
 
+
+
 //perfil
+//aca declaramos la funcion de la controladora que se encarga
+//de manejar la peticion al endpoint
+// • GET /api/auth/perfil
+
+//esta funcion devuelve los datos del usuario logueado
+//recordemos que este enpoint tiene un middleware, a diferencia de los 2 endpoints anteriores
+
+//miremos routes --> auth.routes.js :
+// router.post('/registro', registro);
+// router.post('/login', login);
+// router.get('/perfil', authMiddleware, perfil);
+
+//como vemos /perfil es el unico que pasa por authMiddleware primero
+//asi que antes de llegar aca primero pasa por:
+//middleware --> auth.middleware.js
+
+//recordemos que ese middleware era el “portero de la puerta”
+// y ya hizo esto:
+
+// revisó si traía el token
+// verificó que el token era válido
+// comprobó que no expiró
+// buscó al usuario en la base de datos
+// guardó ese usuario en req.user
+
+// Por eso, cuando llega la peticion a esta funcion /perfil,
+// si logro llegar, es porque ya sabemos con seguridad que:
+
+// el usuario está autenticado
+// el token es válido
+// el usuario existe
+// req.user representa al usuario correcto
+
+//asi que esta funcion perfil no tiene que validar nada, su funcion es hacer esto:
+// • toma req.user (el usuario que viene en la peticion)
+// • limpia los datos con serializeUser (la funcion que hicimos en este mismo archivo)
+// que lo hace es.. de todos los datos obtenidos para ese usuario... de la base de datos
+// vamos a devolver estos campos unicamente.. y dejamos afuera la contraseña
+// asi que con esta funcion no le devolvemos el usuario completo, todo lo que hay
+// para ese usuario en la base de datos, sino los campos que nosotros definimos devolver
+// en esa funcion serializeUser()
+// • asi que finalmente devolvemos esos datos "publicos" (sin la contraseña) al frontend
 function perfil(req, res) {
   return res.json(serializeUser(req.user));
 }
